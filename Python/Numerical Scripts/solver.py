cd0a5eab842196f83a953d5153feda905dabed1d
JurgenTas <jurgen.m.c.tas@gmail.com>
2015-08-12 10:22:43 +0200

    Update solver.py
    Added an 'how-to' example.

--- Python/Numerical Scripts/solver.py
+++ Python/Numerical Scripts/solver.py
@@ -1,7 +1,10 @@
-
 __author__ = 'J Tas'
 
 from scipy import optimize
+import numpy as np
+import matplotlib.pyplot as plt
+
+l = [] # global param list
 
 class Parameter:
     
@@ -13,10 +16,10 @@ class Parameter:
 
     def __call__(self):
         return self.value
-	
+
     def __repr__(self):
-	msg = ("{}(value={value})")
-	return msg.format(self.__class__.__name__,**vars(self))
+        msg = ("{}(value={value})")
+        return msg.format(self.__class__.__name__,**vars(self))
 
 def fit(func, parameters, y, x = None):
     
@@ -30,5 +33,30 @@ def fit(func, parameters, y, x = None):
     if x is None: x = np.arange(y.shape[0])
     p = [param() for param in parameters]
     optimize.leastsq(f, p)
+ 
+def g(x):
+    return l[0]() + l[1]()*x + l[2]()*(x**2.0) + l[3]()*(x**3.0) 
 
+def main():
+    
+    # create data (incl. random noise):
+    x0 = 0
+    x1 = 1
+    n = 100
+    xp = np.linspace(x0, x1, n)
+    yp = np.exp(xp) + np.random.normal(0, 0.1, n)
+    
+    # solve: 
+    l.append(Parameter(0))
+    l.append(Parameter(0))
+    l.append(Parameter(0))
+    l.append(Parameter(0))
+    fit(g, l, yp, xp)
+    
+    # plot results:
+    plt.scatter(xp, yp)
+    plt.plot(xp, g(xp), 'r')
+    plt.show()
 
+if __name__ == "__main__":
+    main()
