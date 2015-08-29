297b45d7a4256cf28ea0d8f9259f5967a01c4862
JurgenTas <jurgen.m.c.tas@gmail.com>
2015-08-29 10:42:15 +0200

    Update priorityqueue.py

--- Python/Algorithms & Data Structures/priorityqueue.py
+++ Python/Algorithms & Data Structures/priorityqueue.py
@@ -1,58 +1,36 @@
 __author__ = 'J Tas'
 
-from mysql.connector import MySQLConnection, Error
-
-db_config = {
-    'user': 'jurgen',
-    'password': 'test',
-    'host': '127.0.0.1',
-    'database': 'test',
-    'raise_on_warnings': True,
-}
+import heapq
 
 
-def query(sql_command):
-    """ Connect to MySQL database """
-    try:
-        print('Connecting to MySQL database...')
-        conn = MySQLConnection(**db_config)
-        cursor = conn.cursor()
-        cursor.execute(sql_command)
-        conn.commit()  # accept the changes
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
 
-        if conn.is_connected():
-            print('connection established.')
-        else:
-            print('connection failed.')
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
 
-    except Error as error:
-        print(error)
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
 
-    finally:
-        cursor.close()
-        conn.close()
-        print('Connection closed.')
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
 
 
 def main():
-    sql_command = """
-    CREATE TABLE employee (
-    staff_number INTEGER PRIMARY KEY,
-    fname VARCHAR(20),
-    lname VARCHAR(30),
-    gender CHAR(1),
-    joining DATE,
-    birth_date DATE);"""
-    query(sql_command)
+    q = PriorityQueue()
+    q.push(Item('foo'), 1)
+    q.push(Item('bar'), 5)
+    q.push(Item('spam'), 4)
+    q.push(Item('grok'), 1)
 
-    staff_data = [("1", "William", "Shakespeare", "m", "1961-10-25"), ("2", "Frank", "Schiller", "m", "1955-08-17"),
-                  ("3", "Jane", "Wall", "f", "1989-03-14")]
-
-    for p in staff_data:
-        format_str = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-        VALUES ({number}, "{first}", "{last}", "{gender}", "{birthdate}");"""
-        sql_command = format_str.format(number=p[0], first=p[1], last=p[2], gender=p[3], birthdate=p[4])
-        query(sql_command)
 
 if __name__ == "__main__":
     main()
