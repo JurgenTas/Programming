b26b730db78d43be2538633a6531a3a2976ff224
jurgentas <jurgen.m.c.tas@gmail.com>
2015-12-05 09:26:42 +0100

    Added neural networks script.

---
+++ Python/Numerical Scripts/neural.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 16:38:51 2015
+
+@author: J. Tas
+"""
+import math
+import numpy as np
+
+# =====================================================================
+
+def logistic(t):
+    """
+    Return logistic(t)
+    """
+    return 1.0 / (1 + math.exp(-t))
+
+def tanh(t):
+    """
+    Return the hyperbolic tangent of t
+    """
+    return math.tanh(t)
+    
+# =====================================================================
+def neuron(func, w, x):
+    return func(np.dot(w, x))
+    
+def feed_forward(network, x, func):
+    """
+    Network represented as a list of lists
+    """
+    res = []
+    for layer in network:
+        x = x + [1] #add bias
+        y = [neuron(func, w, x) for w in layer]
+        res.append(y)
+        x = y  
+    return res
+          
+# =====================================================================
+
+def main():
+	hidden_layer = [[20, 20, -30], [20, 20, -10]]
+	output_layer = [[-60, 60, -30]]
+    network = [hidden_layer,output_layer]
+    for x in [0, 1]:
+        for y in [0, 1]:
+            print(x, y , feed_forward(network, [x, y], tanh)[-1])
+
+# =====================================================================
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Projects/prototype_20151107.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  7 09:16:54 2015
+
+@author: J Tas
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools 
+
+# =====================================================================
+# Globals
+
+items = 5
+score = []                               # scores (to be defined in main)
+alpha =  [1, 1, 1, 1, 1]                 # alpha's
+delta = [-1.9, -0.6, -0.25, 0.30, 0.45]  # deltas's
+
+# =====================================================================
+
+def logistic(x, alpha, delta):
+    """
+    One parameter logistic (1PL) model
+    """
+    arg = np.exp(alpha * (x - delta))
+    return arg / (1 + arg)
+
+def f(x):
+    """
+    Log likelihood function
+    """
+    i, res = 0, 0.0
+    while i < items:
+        arg = logistic(x, alpha[i], delta[i])
+        p = score[i] * np.log(arg) + (1.0 - score[i]) * np.log(1.0 - arg)
+        res += p
+        i += 1
+    return res
+
+def d1f(x, h = 1.e-6):
+    """
+    First derivative: based on central diff. approx.
+    See: https://en.wikipedia.org/wiki/Finite_difference
+    """
+    arg = f(x + h) - f(x - h)
+    return arg / (2 * h)
+
+def d2f(x, h = 1.e-6):
+    """
+    Second derivative: based on central diff. approx.
+    See: https://en.wikipedia.org/wiki/Finite_difference
+    """
+    arg = f(x + h) - 2.0 * f(x) + f(x - h)
+    return arg / (h * h)
+
+# =====================================================================
+
+def plot():
+
+    x0 = -3
+    x1 = 3
+    xp = np.linspace(x0, x1, 100)
+    yp = f(xp)
+    plt.plot(xp, yp, 'r')
+    plt.show()
+
+# =====================================================================
+
+def newton(func, dfunc, x0=1, tol=1E-6, n=20, debug = False):
+    """
+    Newton's algorithm
+    """
+    i = 1
+    while i <= n:
+        x1 = x0 - (func(x0) / dfunc(x0))
+        if debug:
+            print("iteration: ", i, " value: ", x1)
+        if abs(x1 - x0) < tol:
+            return x1
+        else:
+            x0 = x1
+            i += 1
+    return False
+
+# =====================================================================
+
+def main():
+    scores = itertools.product(range(2), repeat=5) # create score combinations
+    global score 
+    for s in scores:   
+        score = s
+        res = newton(d1f, d2f, x0 = 0.0)
+        print("Score vector:", score, "; result:", res)
+    
+if __name__ == "__main__":
+    main()
