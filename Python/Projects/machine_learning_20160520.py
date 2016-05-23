7ac8893b43445b0632129c13541552e72d3dc20c
JurgenTas <jurgen.m.c.tas@gmail.com>
2016-05-23 15:58:33 +0200

    Update machine_learning_20160520.py

--- Python/Projects/machine_learning_20160520.py
+++ Python/Projects/machine_learning_20160520.py
@@ -2,51 +2,61 @@
 __author__ = 'J Tas'
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # =====================================================================
 
 
 class Perceptron(object):
-    def __init__(self, eta=0.1, n=10):
+    """
+    This class implements an algorithm for supervised learning of 
+    binary classifiers. 
+    """
+    def __init__(self, eta=0.01, n=10):
         self.eta = eta
         self.n = n
         self.w = []
-
-
-    def fit(self, xlist, ylist):
+        self.error = []
+    
+    def train(self, xlist, ylist):
         """
-        
-        :type xlist: list of lists
+        :type xlist: list of lists (i.e 2D list)
         :type ylist: list
         """
-        self.w = [0] * len(xlist[0])
+        self.w = [0]*len(xlist[0]) # init. weights
         for _ in range(self.n):
+            cnt = 0
+            print(self.w)
             for x, y in zip(xlist, ylist):
-                error = y - step_function(np.dot(self.w, x))
-                if error != 0:
+                delta = y - self.predict(x) # missclassification: != 0
+                if delta != 0:
+                    cnt += 1
                     for i, xi in enumerate(x):
-                        assert isinstance(xi, int)
-                        self.w[i] += self.eta * error * xi
-                print(self.w)
+                        self.w[i] += self.eta * delta * xi
+            self.error.append(cnt)
+    
 
-
-# =====================================================================
-
-
-def step_function(x):
-    return 0 if x < 0.5 else 1
+    def predict(self, x):
+        return 0 if np.dot(self.w, x) < 0.0 else 1
+    
  
+    def plot_classification_error(self):
+        plt.plot(range(1, len(self.error)+1), self.error, marker='o')
+        plt.xlabel('Iterations')
+        plt.ylabel('Missclassifications')
+        plt.show()
+
     
 # =====================================================================
 
 
 def main():
-    
     x = [[1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]] 
     y = [1, 1, 1, 0] 
     p = Perceptron()
-    p.fit(x, y)
+    p.train(x, y)
+    p.plot_classification_error()
     print(p.w)
 
 
