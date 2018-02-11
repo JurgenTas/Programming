138f6ba7cca9f878fd8c5a4524dc20d5ef815451
jurgentas <jurgen.m.c.tas@gmail.com>
2018-02-11 20:37:25 +0100

    Clean-up of PDE script.

--- Python/Numerical Scripts/pde1.py
+++ Python/Numerical Scripts/pde1.py
@@ -8,53 +8,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Globals:
-M = 100  # Grid squares on a side
-OMEGA = 0.75  # Overelaxation coefficient
+M = 100  # Number of grid points per side
+OMEGA = 0.5  # Parameters to determine over-relaxation
 ITER = 5000  # Maximum number of iterations
 CONST1 = 0.01  # Step size (1 cm)
 CONST2 = 8.854187817e-12  # Vacuum permittivity
+TOL = 1e-6  # Target accuracy
 
 
 def init(n):
-    """Init. phi."""
+    """
+    Init. phi to zeros.
+    """
     phi = np.zeros([n + 1, n + 1], float)
     return phi
 
 
-def f(i, j, c):
+def f(i, j):
     """
     Helper function to model the (square) charges.
     """
     x, y = float(i * CONST1), float(j * CONST1)
     if 0.6 < x < 0.8 and 0.6 < y < 0.8:
-        return c
+        return 1
     elif 0.2 < x < 0.4 and 0.2 < y < 0.4:
-        return -c
+        return -1
     else:
         return 0
 
 
-def solve(phi):
+def solve(x):
     """
-    Solve using the Gauss-Seidel method (using 'overelaxation')
+    Solve using the Gauss-Seidel method (using 'overelaxation').
     """
     errs = []
-    c = ((CONST1 ** 2) / (4 * CONST2))
+    c1 = (CONST1 ** 2) / CONST2
+    c2 = (1 + OMEGA)
     for _ in range(ITER):
         err = 0
         for i in range(1, M):
             for j in range(1, M):
-                diff = (phi[i + 1, j] + phi[i - 1, j] + phi[i, j + 1] + phi[i, j - 1]) / 4 + f(i, j, c) - phi[i, j]
-                phi[i, j] += (1 + OMEGA) * diff
+                diff = (x[i + 1, j] + x[i - 1, j] + x[i, j + 1] + x[i, j - 1] + c1 * f(i, j)) / 4 - x[i, j]
+                x[i, j] += c2 * diff
                 err = max(err, abs(diff))
         errs.append(err)
+        if err < TOL:
+            break
     print("Final error: {} ".format(errs[-1]))
-    return phi, errs
+    return x, errs
 
 
 def main():
     phi0 = init(M)
-    result, errs, = solve(phi0)
+    result, errs = solve(phi0)
     plt.imshow(result, interpolation='bilinear', origin='lower', extent=[0, 1, 0, 1], vmax=abs(result).max(),
                vmin=-abs(result).max())
     plt.show()
