5aeadbfa498496a60ca47201a030dc243ae0b6ab
jurgentas <jurgen.m.c.tas@gmail.com>
2018-02-14 21:21:05 +0100

    Added script to solve 1D heat equation

---
+++ Python/Numerical Scripts/pde2.py
@@ -0,0 +1,67 @@
+"""
+Solves the one-dimensional heat equation (parabolic PDE) using the Crank-Nicholson scheme. This scheme is based on the 
+idea that the forward-in-time approximation of the time derivative is estimating the derivative at the halfway point 
+between times n and n+1. Set-up: The temperature (u) is initially distributed over a one-dimensional, 
+one-unit-long interval.
+"""
+
+import math as mt
+import copy
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.sparse import diags
+
+# Globals:
+M = 100 # Number of spatial slices
+N = 100  # Number of time steps
+D = 1  # thermal diffusivity
+T = 10.0  # number of seconds (s)
+L = float(1)  # size of grid
+
+
+def solve(dx, dt):
+    """
+    Solve using the Crank-Nicholson scheme.
+    """
+
+    # init. lhs and rhs matrices:
+    c = (D / 2.0) * (dt / (dx ** 2))
+    mtrx1 = diags([-c, 2 * (1 + c), -c], [1, 0, -1], shape=(M - 2, M - 2)).toarray()
+    mtrx2 = diags([c, 2 * (1 - c), c], [1, 0, -1], shape=(M - 2, M - 2)).toarray()
+
+    # init. initial temperature distribution:
+    u = [mt.sin(i * dx * mt.pi) for i in range(M)]
+    u[0], u[-1] = 0, 0  # boundary conditions
+    result = [None] * N
+    result[0] = u
+
+    # solve linear system of equations and update rhs:
+    arr = mtrx2.dot(u[1:-1])
+    for i in range(1, N):
+        sol = [0] * M  # init. solution vector
+        sol[1:-1] = np.linalg.solve(mtrx1, arr)
+        arr = mtrx2.dot(sol[1:-1])
+        result[i] = sol
+    return result
+
+
+def main():
+    # init spatial and time step:
+    dx = L / (M - 1)  # grid spacing
+    dt = T / N  # time spacing
+    result = solve(dx, dt)
+
+    x = np.linspace(0, L, M)
+    f1 = result[0]
+    f2 = result[1]
+    f3 = result[2]
+
+    plt.plot(x, f1, 'r')
+    plt.plot(x, f2, 'b')
+    plt.plot(x, f3, 'g')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
